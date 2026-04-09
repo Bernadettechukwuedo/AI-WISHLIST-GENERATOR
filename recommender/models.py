d8fe025298ac606a8979bf634ea6fd7e87e39b8e
Bernadette Chukwuedo <chukwuedoogochukwubernadette@gmail.com>
2026-04-09 13:19:43 +0100

    configuration for deployment

--- recommender/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Prediction(models.Model):
-    age = models.IntegerField()
-    gender = models.CharField(max_length=10)
-    category = models.IntegerField()
-    created_at = models.DateTimeField(auto_now_add=True)
